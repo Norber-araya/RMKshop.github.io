c63e25f511bc0796f40a9d8058d0b202306b4b43
sebhacdls <sebastian.varelaleon@gmail.com>
2023-05-03 18:25:17 -0400

    Probando hamburguesa

---
+++ script.js
@@ -0,0 +1,12 @@
+// const hamburguesa = document.querySelector(".hamburguesa");
+// const navLink = document.querySelector(".navLink");
+
+// hamburguesa.addEventListener("click", () => {
+//   hamburguesa.classList.toggle("active");
+//   navLink.classList.toggle("active");
+// })
+
+//  document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+//   hamburguesa.classList.remove("active");
+//   navLink.classList.remove("active");
+//  }))
