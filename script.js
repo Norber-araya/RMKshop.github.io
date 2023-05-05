bd5427ebb820dd4405cc237ab735724b1e2f6716
sebhacdls <sebastian.varelaleon@gmail.com>
2023-05-05 16:27:22 -0400

    Era del signo libra

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
    navLink.classList.toggle("active");
  })
 
- document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
    hamburguesa.classList.remove("active");
-   navLink.classList.remove("active");
-  }))
+    navLink.classList.remove("active");
+   }))
